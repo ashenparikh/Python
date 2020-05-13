acf57d187cf7ae7b4477b1ae18a24b62851407bc
ashenparikh <ashenparikh@g.ucla.edu>
2020-05-12 17:30:10 -0700

    Delete PostgresConnectionModule.py

--- PythonKP/Projects/Dash/Dash-PostGres-Plot/PostgresConnectionModule.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(host=$host,database=$database, user=$user, password=$passwrd)
-cursor = conn.cursor()
-cursor.execute("""select * from information_schema.tables""")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
